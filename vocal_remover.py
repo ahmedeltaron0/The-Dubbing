33f7e84ffe6ed6ea6dc0676c8e501cb1b85ab910
Ahmed Elsayed Eltaron <96958864+ahmedeltaron0@users.noreply.github.com>
2024-02-11 09:38:15 +0200

    add vocal remover file

--- vocal_remover.py
+++ vocal_remover.py
@@ -9,21 +9,24 @@ def separate_vocals(input_file, output_vocals, output_music):
         # Separate vocals and accompaniment
         separator.separate_to_file(input_file, output_music)
 
-       # Rename the generated accompaniment file to output_music
-        os.rename(os.path.join(output_music, 'accompaniment.wav'), os.path.join(output_music, 'output_accompaniment.wav'))
-
-    # Rename the generated vocals file to output_vocals
-        os.rename(os.path.join(output_music, 'vocals.wav'), os.path.join(output_music, 'output_vocals.wav'))
-
-        return output_vocals, output_music
+        # Check if the generated files exist
+        vocals_file = os.path.join(output_music, 'vocals.wav')
+        music_file = os.path.join(output_music, 'accompaniment.wav')
+        if os.path.exists(vocals_file) and os.path.exists(music_file):
+            # Rename the generated files with appropriate filenames
+            os.rename(vocals_file, os.path.join(output_vocals, 'output_vocals.wav'))
+            os.rename(music_file, os.path.join(output_music, 'output_music.wav'))
+            return os.path.join(output_vocals, 'output_vocals.wav'), os.path.join(output_music, 'output_music.wav')
+        else:
+            raise FileNotFoundError("Generated files not found.")
     except Exception as e:
         print("An error occurred:", e)
         return None, None
 
 if __name__ == "__main__":
     input_file = r'output_audio/ولاد رزق 8ث.wav'
-    output_vocals = r'output_audio/vocals'
-    output_music = r'output_audio/music'
+    output_vocals = r'output_speelter/vocals'
+    output_music = r'output_speelter/music'
 
     vocals_file, music_file = separate_vocals(input_file, output_vocals, output_music)
 
